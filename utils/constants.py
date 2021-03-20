f91f2e641e943c9878a399ea7b144b1a914cafa7
Arcane97 <haker_ilnur_97@mail.ru>
2021-03-20 20:36:44 +0500

    Изменил получение строки пути к корню проекта

--- utils/constants.py
+++ utils/constants.py
@@ -1,10 +1,7 @@
-import sys
+from definitions import ROOT_DIR
 
-ROOT_PATH = sys.path[1]  # todo потестить точно ли это кореть проекта
-
-SETTINGS_FILE_NAME = ROOT_PATH + "\\discrete_binance_exchange_settings.json"
+SETTINGS_FILE_NAME = ROOT_DIR + "\\discrete_binance_exchange_settings.json"
 
 
 if __name__ == "__main__":
-    print(sys.path[1])
     print(SETTINGS_FILE_NAME)
